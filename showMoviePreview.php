caf768ae85cb5ab820213c0e590d9fb671eab217
solid-snap <102662688+solid-snap@users.noreply.github.com>
2023-06-06 13:44:13 +0200

    inhoud toevoeging

--- showMoviePreview.php
+++ showMoviePreview.php
@@ -9,7 +9,20 @@
         <li><a href="videoSearch.php">trailer</a></li>
         <li><a href="login.php">login</a></li>
     </ul>
+
 </nav>
+    <?php
+    require_once "show/shows.php";
+    $shows1 = new shows();
+    $shows1->readshows();
+
+    ?>
+    <br>
+    <?php
+    require_once "movies/Movies.php";
+    $mov1 = new Movies();
+    $mov1->readMov();
+    ?>
 <a href="index.php">go to home screen</a>
 </div>
 </body>
--- videoSearch.php
+++ videoSearch.php
@@ -1,5 +1,8 @@
 <html>
 <body>
+<?php
+include "api.php";
+?>
 <link rel="stylesheet" href="hitlist.css">
 <div class="pageInfo">
 <nav>
@@ -10,6 +13,10 @@
         <li><a href="login.php">login</a></li>
     </ul>
 </nav>
+    <h2> click on the spyglass to search for a trailer</h2>
+        <a href="https://www.youtube.com/@RottenTomatoesTRAILERS"><img src="img/spyv2.png"></a>
+
+    <br>
 <a href="index.php">go to home screen</a>
 </div>
 </body>
