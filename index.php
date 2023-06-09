54ff85f792455504e2154f8784e0d22b8ed353f9
solid-snap <102662688+solid-snap@users.noreply.github.com>
2023-06-09 15:37:17 +0200

    .

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
         <nav>
             <ul>
                 <li><a href="index.php" class="active">Home</a></li>
-                <li><a href="showMoviePreview.php">Movies-Shows</a></li>
-                <li><a href="videoSearch.php">trailer</a></li>
-                <li><a href="login.php">login</a></li>
+                <li><a href="userMenus/showMoviePreview.php">shows&movies</a></li>
+                <li><a href="userMenus/videoSearch.php">trailer</a></li>
+                <li><a href="login/login.php">login</a></li>
             </ul>
         </nav>
     </div>
