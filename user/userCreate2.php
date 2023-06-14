e3734bea817a9392cbfad906e0890d8c2287a99a
solid-snap <102662688+solid-snap@users.noreply.github.com>
2023-06-14 09:03:41 +0200

    Update userCreate2.php

--- user/userCreate2.php
+++ user/userCreate2.php
@@ -11,18 +11,21 @@
     </div>
         <?php
         require "User.php";
-
-        // uitlezen vakjes van KlantenCreate1 -----
         $userName=$_POST["userNameVak"];
-        $userPassword=$_POST["userPasswordVak"];
+        $password=$_POST["userPasswordVak"];
+
+        $gehashed = password_hash($password, null);
+        $userPassword =$gehashed;
+
         // maken object -------------------------------
         $user1 = new User($userName, $userPassword);
         $user1->createUser();
 
         // afdrukken object ---------------------------
 
-        $user1->afdrukken();
         ?>
+        <h2><a href="../login/login.php">go back to login</a></h2>
+    </div>
     </body>
 </html>
 
